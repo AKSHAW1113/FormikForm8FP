075d4962bf50b08195deb39a4f737a0fbe892feb
AKSHAW1113 <pyexpert62@gmail.com>
2026-07-09 18:24:14 +0530

    Update UseRefDisplayData.jsx
    changes

--- src/components/UseRefDisplayData.jsx
+++ src/components/UseRefDisplayData.jsx
@@ -1,46 +1,43 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState } from 'react'
 
-function Form() {
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const [data, setData] = useState([]);
+function Parent() {
+  let nameRef = useRef(null)
+  let emailRef = useRef(null)
+  const [data, setData] = useState([])
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    const newEntry = {
+      name: nameRef.current.value.trim(),
+      email: emailRef.current.value.trim()
+    }
+    if (!name || !email) {
+      alert("Please fill all fields.");
+      return;
+    }
+    // console.log(newEntry);
+    setData(prev => [...prev, newEntry])
+    nameRef.current.value = ""
+    emailRef.current.value = ""
+  }
 
-    const formData = {
-      name: nameRef.current.value,
-      email: emailRef.current.value,
-    };
+  console.log("data: ", data);
 
-    setData((prev) => [...prev, formData]);
-    nameRef.current.value = "";
-    emailRef.current.value = "";
-  };
 
   return (
-    <div className="p-5">
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <div className='p-5'>
+      <form onSubmit={handleSubmit} className='space-y-4'>
         <div>
-          <label>Name: </label>
-          <input type="text" ref={nameRef} placeholder="Enter Name" className="border px-3 py-2 ml-2" />
+          <label>Enter Name</label>
+          <input type="text" ref={nameRef} placeholder='Enter Name' className='px-4 py-2 border-slate-50 border-2 ml-4' />
         </div>
-
         <div>
-          <label>Email: </label>
-          <input type="email" ref={emailRef} placeholder="Enter Email" className="border px-3 py-2 ml-2"
-          />
+          <label>Enter Email</label>
+          <input type="email" ref={emailRef} placeholder='Enter Email' className='px-4 py-2 border-slate-50 border-2 ml-4' />
         </div>
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Submit
-        </button>
+        <button type='Submit' className='bg-purple-600 px-4 py-2.5'>Submit</button>
       </form>
 
-      {/* Display all submissions */}
       <div className="mt-5">
         <h2 className="text-xl font-bold mb-2">Submitted Data</h2>
 
@@ -56,7 +53,7 @@ function Form() {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default Form; 
+export default Parent
